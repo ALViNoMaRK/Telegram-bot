979d5733cfeec40c8098760c56a63d6a426e6724
ALViNoMaRK <alvinomark29@gmail.com>
2025-11-23 00:06:55 +0530

    Add Telegram notifier function

---
+++ telegram/notifier.py
@@ -0,0 +1,6 @@
+import requests
+from config import TELEGRAM_BOT_TOKEN, TELEGRAM_CHAT_ID
+
+def notify(msg):
+    url = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage"
+    requests.post(url, json={"chat_id": TELEGRAM_CHAT_ID, "text": msg})
